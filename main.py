2f922d3688e6ddac957f8bad2875cdef449f9650
FrK5E <FrK5Ewzb4XbBN1aWlGUZ@proton.me>
2023-04-16 22:06:53 +0200

    introduce tests, make porgress

--- main.py
+++ main.py
@@ -11,25 +11,50 @@ def get_values_rank():
 
 values_rank = get_values_rank()
 
-def detect_tuple( hand: str ):
-    items = hand.split() 
-    result = []
-    for (i,c) in enumerate(items[:-1]):
+Card = str
+
+def detect_tuple( cards: list[Card] ):
+    result = [] 
+    for (i,c) in enumerate(cards[:-1]):
         n = 1
-        for d in items[i+1:]: 
+        for d in cards[i+1:]: 
             if c[0]==d[0]:
                 n=n+1
         if n>1:
             result.append( (n, c[0] ))
     return result
 
-def sort_cards( hand: str ): 
-    items = hand.split()
-    return sorted( items, key= lambda a: values_rank[a[0]]) 
+def sort_cards( hand: list[Card] ) -> list[Card]: 
+    return sorted( hand, key= lambda a: values_rank[a[0]]) 
+
+def is_flush( hand: list[Card] ) -> bool: 
+    first_card_suite = hand[0][1]
+    for ( i, c) in enumerate( hand[1:]):
+        if i[1]!=first_card_suite: 
+            return False  
+    return True
+
+def is_straight( hand: list[Card]) -> bool: 
+    k = values_rank[hand[0][0]]
+    for (i,c) in enumerate( hand[1:]):
+        k1 = values_rank[c[0]]
+        if k+i+1 != k1: 
+            return False
+    return True
 
 
 
-                
+
+import sys
+import unittest
+
+class Tests(unittest.TestCase):
+
+    def test_sort1(self):
+        hand = "JC QS JH 2C 6S".split()
+        hand1 = sort_cards( hand )
+        self.assertEqual(hand1, "2C 6S JC JH QS".split())
+
 
 
 #def get_rank( hand: str ): 
@@ -47,26 +72,32 @@ def player_1_wins( line ):
 
     k = len(line) // 2
 
-    hand1 = line[0:k]
-    hand2 = line[k:]
+    list1 = line[0:k]
+    list2 = line[k:]
 
-    k1 = sort_cards( hand1 )
-    k2 = sort_cards( hand2 )
+    k1 = sort_cards( list1.split() )
+    k2 = sort_cards( list2.split() )
 
-    print( "hand1", hand1, "sorted_hand_1", k1 )
-    print( "hand1", hand1, detect_tuple(hand1), "hand2", hand2, detect_tuple(hand2))
+    print( "hand1", list1, "sorted_hand_1", k1 )
+    print( "hand1", list1, detect_tuple(list1), "hand2", list2, detect_tuple(list2))
     
 
     return 1
 
+if __name__=="__main__":
 
-f = open('p054_poker.txt')
-lines = f.readlines()
-
-
-count = 0
-for l in lines[0:50]: 
-    count = count + player_1_wins( l )
+    r = unittest.main( exit=False, failfast=True)
+    if len(r.result.failures)>0: 
+        sys.exit(1) 
 
 
-print( count )
+    f = open('p054_poker.txt')
+    lines = f.readlines()
+
+
+    count = 0
+    for l in lines[0:50]: 
+        count = count + player_1_wins( l )
+
+
+    print( count )
